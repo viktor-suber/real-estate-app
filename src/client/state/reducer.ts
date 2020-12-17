1075320f003a5a291398b14d50564a2feba09d6e
Viktor Suber <viktor.suber@gmail.com>
2020-12-16 22:43:59 -0800

    add data retrieval to reducer

--- src/client/state/reducer.ts
+++ src/client/state/reducer.ts
@@ -1,7 +1,13 @@
-import { Action } from "./actions";
+import { Action, ActionTypes } from "./actions";
 import { AppState } from "./AppState"
 
 const reducer = (state: AppState, action: Action): AppState => {
+  if (action.type === ActionTypes.HOMES_LOADED) {
+    return {
+      ...state,
+      homes: action.payload
+    };
+  } 
   return state;
 };
 
