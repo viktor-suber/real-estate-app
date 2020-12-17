f1911ac78932278bc7c6ff326c92b27da2c57c02
Viktor Suber <viktor.suber@gmail.com>
2020-12-17 14:39:49 -0800

    begin homes filter form

--- src/client/shared/components/Header.tsx
+++ src/client/shared/components/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import HomesFilterForm from "./HomesFilterForm";
 // import { Context } from "../../state/context";
 
 const Header: React.FC = () => {
@@ -7,7 +8,7 @@ const Header: React.FC = () => {
   return (
     <div className="navbar navbar-dark bg-dark navbar-expand-sm navbar-expand-md navbar-expand-lg navbar-expand-xl">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Search"><span className="navbar-toggler-icon"></span></button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">surprise</div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent"><HomesFilterForm /></div>
     </div>
   );
 };
---
+++ src/client/shared/components/HomesFilterForm.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+// import { Context } from "../../state/context";
+
+const HomesFilterForm: React.FC = () => {
+  // const { appData } = useContext(Context);
+
+  return (
+    <form>
+      <div className="form-group">
+      </div>
+    </form>
+  );
+};
+
+export default HomesFilterForm;
