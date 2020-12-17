0334820384146562e20679ff44f63b141961b0b5
Viktor Suber <viktor.suber@gmail.com>
2020-12-17 15:26:26 -0800

    basic form

--- src/client/shared/components/Header.tsx
+++ src/client/shared/components/Header.tsx
@@ -1,14 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import HomesFilterForm from "./HomesFilterForm";
-// import { Context } from "../../state/context";
 
 const Header: React.FC = () => {
-  // const { appData } = useContext(Context);
-
   return (
-    <div className="navbar navbar-dark bg-dark navbar-expand-sm navbar-expand-md navbar-expand-lg navbar-expand-xl">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Search"><span className="navbar-toggler-icon"></span></button>
+    // TODO - IMPLEMENT COLLAPSE
+    <div className="navbar navbar-light bg-light navbar-expand">
+      <div className="container-fluid">
+      {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Search"><span className="navbar-toggler-icon"></span></button> */}
       <div className="collapse navbar-collapse" id="navbarSupportedContent"><HomesFilterForm /></div>
+      </div>
     </div>
   );
 };
--- src/client/shared/components/HomesFilterForm.tsx
+++ src/client/shared/components/HomesFilterForm.tsx
@@ -5,9 +5,32 @@ const HomesFilterForm: React.FC = () => {
   // const { appData } = useContext(Context);
 
   return (
-    <form>
-      <div className="form-group">
+    <form autoComplete="off">
+      <div className="form-group form-row">
+        <div className="col">
+          <label htmlFor="location">Location</label>
+          <input type="text" className="form-control" id="location"></input>
+        </div>
+        <div className="col">
+          <label htmlFor="minPrice">Min Price</label>
+          <input type="text" className="form-control" id="minPrice"></input>
+        </div>
+        <div className="col">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input type="text" className="form-control" id="maxPrice"></input>
+        </div>
+        <div className="col">
+          <label htmlFor="minBedrooms">Min Bedrooms</label>
+          <input type="text" className="form-control" id="minBedrooms"></input>
+        </div>
+        <div className="col">
+          <label htmlFor="maxBedrooms">Max Bedrooms</label>
+          <input type="text" className="form-control" id="maxBedrooms"></input>
+        </div>
       </div>
+      <button type="submit" className="btn btn-primary">
+        Search
+      </button>
     </form>
   );
 };
