871012f99eecd65d784e8b77ef24e1a68340dd04
Viktor Suber <viktor.suber@gmail.com>
2020-12-17 11:32:43 -0800

    bootstrap grid

--- src/client/views/HomesList.tsx
+++ src/client/views/HomesList.tsx
@@ -1,17 +1,21 @@
-import React, { useContext } from 'react';
-import HomeCard from '../shared/components/HomeCard';
-import { Context } from '../state/context';
+import React, { useContext } from "react";
+import HomeCard from "../shared/components/HomeCard";
+import { Context } from "../state/context";
 
 const HomesList: React.FC = () => {
   const { appData } = useContext(Context);
   const { homes } = appData || {};
 
   return (
-    <>
-      {homes.map((home: any) => {
-        return <HomeCard key={home.id} homeInfo={home.property} />;
-      })}
-    </>
+    <div className="py-5">
+      <div className="container">
+        <div className="row">
+            {homes.map((home: any) => {
+              return <div className="col"><HomeCard key={home.id} homeInfo={home.property} /></div>;
+            })}
+        </div>
+      </div>
+    </div>
   );
 };
 
