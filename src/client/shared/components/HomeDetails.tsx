a869b189d1d67e7eae3e9ec3b50769686dd387fc
Viktor Suber <viktor.suber@gmail.com>
2020-12-18 00:48:09 -0800

    display home details for each route

--- src/client/shared/components/HomeDetails.tsx
+++ src/client/shared/components/HomeDetails.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Context } from "../../state/context";
 
 interface URLParam {
   homeId: string;
 }
 
-
 const HomeDetails: React.FC = () => {
+  const { appData } = useContext(Context);
   const { homeId } = useParams<URLParam>();
+
+  const { homes } = appData || {};
+  let currentHome;
+  
+  homes.every((home: any) => {
+    if (home.id === parseInt(homeId))  {
+      currentHome = home;
+      return false;
+    }
+    return true;
+  });
 
   return (
     <>
-      info { homeId }
+      info { JSON.stringify(currentHome) }
     </>
   );
 };
