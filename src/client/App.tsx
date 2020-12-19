d42530eb3682e6648ba761d44fbbacba22c32d8c
Viktor Suber <viktor.suber@gmail.com>
2020-12-19 14:41:10 -0800

    back link

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,38 +1,47 @@
-import React, { useContext } from 'react';
-import { Context } from './state/context';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './shared/components/Header';
-import HomesList from './views/HomesList';
-import HomeDetails from './shared/components/HomeDetails/HomeDetails';
-import HomesFilterForm from './shared/components/HomesFilterForm';
+import React, { useContext } from "react";
+import { Context } from "./state/context";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Header from "./shared/components/Header";
+import HomesList from "./views/HomesList";
+import HomeDetails from "./shared/components/HomeDetails/HomeDetails";
+import HomesFilterForm from "./shared/components/HomesFilterForm";
 
 const App: React.FC = () => {
-
   const { appData } = useContext(Context);
   const { loading, error } = appData || {};
 
   return (
     <div className="App">
-        {loading ? <p>Loading...</p> : (
-          <>
-            {!error ? (
-              <Router>
-                <Switch>
-                  <Route path="/homes">
-                  <Header>test</Header>
-                    <HomesList />
-                  </Route>
-                  <Route path="/">
-                  <Header><HomesFilterForm /></Header>
-                    <HomesList />
-                  </Route>
-                </Switch>
-              </Router>
-            ) : `An error occurred: ${error}`}
-          </>
-        )}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {!error ? (
+            <Router>
+              <Switch>
+                <Route path="/homes">
+                  <Header>
+                    <div className="pb-2">
+                      <Link to={"/"}>Back</Link>
+                    </div>
+                  </Header>
+                  <HomesList />
+                </Route>
+                <Route path="/">
+                  <Header>
+                    <HomesFilterForm />
+                  </Header>
+                  <HomesList />
+                </Route>
+              </Switch>
+            </Router>
+          ) : (
+            `An error occurred: ${error}`
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
