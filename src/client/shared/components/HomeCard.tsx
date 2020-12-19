fafa0ecde002eea07cb8001e6c4aa9be036c6dfb
Viktor Suber <viktor.suber@gmail.com>
2020-12-19 07:32:27 -0800

    more card styling

--- src/client/shared/components/HomeCard.tsx
+++ src/client/shared/components/HomeCard.tsx
@@ -6,21 +6,47 @@ interface HomeCardProps {
 }
 
 const HomeCard: React.FC<HomeCardProps> = ({ homeInfo, price }) => {
-
-  const { primaryImageUrl, address, squareFeet, numberBedrooms, numberBaths } = homeInfo;
+  const {
+    description,
+    primaryImageUrl,
+    address,
+    squareFeet,
+    numberBedrooms,
+    numberBaths,
+  } = homeInfo;
 
   const { addressLine1, city, state } = address;
 
   return (
     <div className="card mb-2">
-      <img src={primaryImageUrl} className="card-img-top" alt="..." />
+      <span className="card-price h4 p-2 m-2">
+        ${price}
+      </span>
+      <img src={primaryImageUrl} className="card-img-top home-card-img" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">{addressLine1} {city}, {state} - ${price}</h5>
-        <p className="card-text">
-        { numberBedrooms } Bedrooms<br/>
-        { numberBaths } Bathrooms<br/>
-        { squareFeet } Square Feet
-        </p>
+      <h5 className="card-title">
+        {addressLine1} {city}, {state}
+      </h5>
+      <p>
+        { description }
+      </p>
+      <div className="row border-top pt-3">
+          <div className="col-sm">
+            {numberBedrooms}
+            <br />
+            Bedrooms
+          </div>
+          <div className="col-sm">
+            {numberBaths}
+            <br />
+            Baths
+          </div>
+          <div className="col-sm">
+            {squareFeet}
+            <br />
+            Square Feet
+          </div>
+        </div>
       </div>
     </div>
   );
