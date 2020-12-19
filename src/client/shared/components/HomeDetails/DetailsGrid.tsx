39fc12bf0959332e1a9e9741520a678ade6424ff
Viktor Suber <viktor.suber@gmail.com>
2020-12-19 08:12:32 -0800

    update details page grid

--- src/client/shared/components/HomeDetails/DetailsGrid.tsx
+++ src/client/shared/components/HomeDetails/DetailsGrid.tsx
@@ -5,7 +5,14 @@ interface DetailsGridProps {
 }
 
 const DetailsGrid: React.FC<DetailsGridProps> = ({ currentHome }) => {
-  const { property, price, includedItems, excludedItems, escrowCompany, titleCompany } = currentHome;
+  const {
+    property,
+    price,
+    includedItems,
+    excludedItems,
+    escrowCompany,
+    titleCompany,
+  } = currentHome;
 
   const {
     address,
@@ -14,7 +21,7 @@ const DetailsGrid: React.FC<DetailsGridProps> = ({ currentHome }) => {
     numberBedrooms,
     numberBaths,
     squareFeet,
-    propertyType
+    propertyType,
   } = property;
 
   const { addressLine1, addressLine2, city, state, zip } = address;
@@ -23,46 +30,52 @@ const DetailsGrid: React.FC<DetailsGridProps> = ({ currentHome }) => {
     <div className="container py-4">
       <div className="row">
         <div className="col">
-          <h4>{`${addressLine1} ${
-            addressLine2 ? addressLine2 : ""
-          } - ${city}, ${state} ${zip} - $${price}`}</h4>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <img src={primaryImageUrl} className="img-fluid" alt="..." />
+          <img src={primaryImageUrl} className="details-img" alt="..." />
         </div>
         <div className="col">
           <div className="row">
             <div className="col-sm">
               <h6>Description</h6>
-              <p>{description}</p>
+              <p className="lead">{description}</p>
+            </div>
+          </div>
+          <div className="row">
+          <div className="col-sm">
+              <h6>Price</h6>
+              <span className="lead">${price}</span>
+            </div>
+            <div className="col-sm">
+              <h6>Address</h6>
+              <p className="lead">
+              {addressLine1}
+              {addressLine2 ? (<><br />{addressLine2}</>) : null}
+              <br /> {city}, {state} {zip}
+              </p>
+
             </div>
           </div>
           <div className="row">
             <div className="col-sm">
               <h6>Bedrooms</h6>
-              {numberBedrooms}
+              <span className="lead">{numberBedrooms}</span>
             </div>
             <div className="col-sm">
               <h6>Bathrooms</h6>
-              {numberBaths}
+              <span className="lead">{numberBaths}</span>
             </div>
             <div className="col-sm">
               <h6>Square Feet</h6>
-              {squareFeet}
+              <span className="lead">{squareFeet}</span>
             </div>
           </div>
           {includedItems.length || excludedItems.length ? (
-            <div className="row">
+            <div className="row mt-4">
               {includedItems.length ? (
                 <div className="col-sm">
                   <h6>Included:</h6>
                   <ul>
                     {includedItems.map((item: any) => {
-                      return (
-                        <li key={item.id}>{item.name}</li>
-                      );
+                      return <li key={item.id}>{item.name}</li>;
                     })}
                   </ul>
                 </div>
@@ -72,35 +85,35 @@ const DetailsGrid: React.FC<DetailsGridProps> = ({ currentHome }) => {
                   <h6>Not Included:</h6>
                   <ul>
                     {excludedItems.map((item: any) => {
-                      return (
-                        <li key={item.id}>{item.name}</li>
-                      );
+                      return <li key={item.id}>{item.name}</li>;
                     })}
                   </ul>
                 </div>
               ) : null}
             </div>
           ) : null}
-          <div className="row">
+          <div className="row mb-3">
             <div className="col-sm">
               <h6>Type</h6>
-              { propertyType.match(/[A-Z][a-z]+/g).join(' ') }
+              {propertyType.match(/[A-Z][a-z]+/g).join(" ")}
             </div>
             <div className="col-sm">
               <h6>Status</h6>
-              { currentHome.state }
+              {currentHome.state}
             </div>
           </div>
           <div className="row">
             <div className="col-sm">
               <h6>Escrow Company</h6>
-              { escrowCompany.name }<br/>
-              Officer: { escrowCompany.officerName }
+              {escrowCompany.name}
+              <br />
+              Officer: {escrowCompany.officerName}
             </div>
             <div className="col-sm">
               <h6>Title Company</h6>
-              { titleCompany.name }<br/>
-              Officer: { titleCompany.officerName }
+              {titleCompany.name}
+              <br />
+              Officer: {titleCompany.officerName}
             </div>
           </div>
         </div>
