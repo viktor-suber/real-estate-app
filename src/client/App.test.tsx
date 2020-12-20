8644596f4f4ad97b722ebcce14dbe0eaa3777fb2
Viktor Suber <viktor.suber@gmail.com>
2020-12-19 16:32:25 -0800

    update

--- src/client/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
-
-test('renders App component', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
